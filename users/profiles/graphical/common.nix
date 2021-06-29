f6272a7daa20911d8398abb2b5f8ba07157c3ed7
Valts Liepiņš <valts.liepins@pm.me>
2021-06-29 17:26:39 +0300

    Adds kitty configuration

--- users/profiles/graphical/common.nix
+++ users/profiles/graphical/common.nix
@@ -2,5 +2,6 @@
   imports = [
     ./xmobar
     ./xmonad
+    ./kitty
   ];
 }
---
+++ users/profiles/graphical/kitty/default.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ kitty ];
+
+  home.file = {
+    ".config/kitty/kitty.conf".source = ./kitty.conf;
+  };
+}
