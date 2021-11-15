822bfecc53d13a8ecc1a4f0dea52afdb419464f2
Valts Liepiņš <valts.liepins@pm.me>
2021-11-15 11:27:05 +0200

    Adds my cachix binary cache

---
+++ profiles/cachix/cipharius.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://cipharius.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "cipharius.cachix.org-1:g/0MTHiKffFGd4bnVWX2E3rXqXL3xfjkYlCgq8/5PvM="
+    ];
+  };
+}
