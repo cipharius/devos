2c06bd76950d8d25c9541a2a43d6cc70a6fc3d5e
Valts Liepiņš <valts.liepins@pm.me>
2021-12-21 07:57:56 +0200

    Configures static IP for homeserver

--- hosts/armarium.nix
+++ hosts/armarium.nix
@@ -13,8 +13,18 @@
   boot.kernelModules = [ "kvm-intel" ];
 
   networking.useDHCP = false;
-  networking.interfaces.eno1.useDHCP = true;
-  networking.interfaces.wlo1.useDHCP = true;
+  networking.interfaces.eno1.useDHCP = false;
+  networking.interfaces.wlo1.useDHCP = false;
+  networking.interfaces.wlo1.ipv4.addresses = [ {
+    address = "192.168.8.10";
+    prefixLength = 24;
+  } ];
+  networking.defaultGateway = "192.168.8.1";
+  networking.nameservers = [
+    "194.8.2.2",
+    "9.9.9.9",
+    "8.8.8.8"
+  ];
 
   time.timeZone = "Europe/Riga";
 
