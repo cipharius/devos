3b8b82fcb2957cc4a25085ba236f05f0bc4beb1d
Valts Liepiņš <valts.liepins@pm.me>
2021-12-28 12:38:28 +0200

    Fixes paperless configuration

--- profiles/server/paperless-ng/default.nix
+++ profiles/server/paperless-ng/default.nix
@@ -2,7 +2,7 @@
   services.paperless.enable = true;
   services.paperless.package = "pkgs.paperless-ng";
   services.paperless.port = 28981;
-  services.ocrLanguages = [ "eng" "lav" ];
+  services.paperless.ocrLanguages = [ "eng" "lav" ];
   services.paperless.extraConfig = {
     PAPERLESS_AUTO_LOGIN_USERNAME = "admin";
   };
