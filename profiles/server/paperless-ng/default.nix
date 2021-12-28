a6dcff8f595d7399a8097a11c033d18a51661891
Valts Liepiņš <valts.liepins@pm.me>
2021-12-28 12:39:33 +0200

    Fixes paperless package

--- profiles/server/paperless-ng/default.nix
+++ profiles/server/paperless-ng/default.nix
@@ -1,6 +1,6 @@
-{ ... }: {
+{ ..., pkgs }: {
   services.paperless.enable = true;
-  services.paperless.package = "pkgs.paperless-ng";
+  services.paperless.package = pkgs.paperless-ng;
   services.paperless.port = 28981;
   services.paperless.ocrLanguages = [ "eng" "lav" ];
   services.paperless.extraConfig = {
